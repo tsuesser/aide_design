0c4f7ed265e66b548dd069a14d4a78edc88dc506
ethan92429 <ethan.keller@gmail.com>
2017-08-24 12:42:42 -0600

    uggh, a comma

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.abspath('../../aide_design'))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.imgmath', 'sphinx.ext.napoleon',
               'sphinx.ext.intersphinx', 'sphinx.ext.coverage',
               'sphinx.ext.doctest', 'sphinx.ext.autosummary',
-              'sphinx.ext.graphviz',]
+              'sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
